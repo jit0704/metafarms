b7f29c5a6bbaf1e805b406e693b98e1b14e54978
jeon <foodchain@naver.com>
2022-06-10 00:48:36 +0900

    관리자 : 1. 로그인 화면 및 메인 템플릿 화면 작업

---
+++ admin/js/common/ui.js
@@ -0,0 +1,14 @@
+$(function(){
+  // gnb 메뉴
+  $('.ly-header .ui.dropdown').dropdown({
+    on: 'hover',
+    action: 'nothing',
+    delay: {
+      hide: 100,
+      show: 100,
+    }
+  });
+
+  // 필터 컨테이너 셀렉트 박스
+  $('.fliter-container .ui.dropdown').dropdown();
+});
