e72784fb2dc564fe538a411b84317f1a40638a1d
jeon <foodchain@naver.com>
2022-07-18 23:37:41 +0900

    모웹: 상품 구매 화면(일반상품/마이닝상품) 작업

--- pub/mobileweb/js/common/ui.js
+++ pub/mobileweb/js/common/ui.js
@@ -8,4 +8,29 @@ $(function(){
       $('#header-title').text(includeHtml);
     }, 10);
   }
+
+  // ui 인터렉션 호출
+  cmmnUi.init();
 });
+
+const cmmnUi = {
+  init () {
+    cmmnUi.btnGroupTab(); 
+  },
+  btnGroupTab () { // 구매/자산관리 탭 메뉴
+    $('.purchase-asset-group > .btn-group').on('click', '.btn-block', function () {
+      var $this = $(this);
+      var dataMate = $this.attr('data-mate');
+      $this.parent().addClass('tab');
+      $('.btn-group.tab .btn-block').removeClass('on');
+      $this.addClass('on');
+      $('.item-goods-buy').hide();
+      $(`.item-goods-buy[data-mate=${dataMate}]`).show();
+    });
+  
+    $('.purchase-asset-group > .btn-group').on('click', '.btn-block.on', function () {
+      $(this).removeClass('on').parent().removeClass('tab');
+      $('.purchase-asset-group .item-goods-buy').hide();
+    });
+  }
+};
